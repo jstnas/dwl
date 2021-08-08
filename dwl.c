c9e9b3d0a626459e8357ead96ffa9ed1e36a7727
Justinas Grigas <jstn_as@protonmail.com>
2021-08-08 12:17:59 +0100

    launched terminal is now st

--- dwl.c
+++ dwl.c
@@ -53,15 +53,15 @@
 /* macros */
 #define BARF(fmt, ...)		do { fprintf(stderr, fmt "\n", ##__VA_ARGS__); exit(EXIT_FAILURE); } while (0)
 #define EBARF(fmt, ...)		BARF(fmt ": %s", ##__VA_ARGS__, strerror(errno))
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define CLEANMASK(mask)         (mask & ~WLR_MODIFIER_CAPS)
-#define VISIBLEON(C, M)         ((C)->mon == (M) && ((C)->tags & (M)->tagset[(M)->seltags]))
-#define LENGTH(X)               (sizeof X / sizeof X[0])
-#define END(A)                  ((A) + LENGTH(A))
-#define TAGMASK                 ((1 << LENGTH(tags)) - 1)
-#define ROUND(X)                ((int)((X)+0.5))
-#define LISTEN(E, L, H)         wl_signal_add((E), ((L)->notify = (H), (L)))
+#define MAX(A, B)			((A) > (B) ? (A) : (B))
+#define MIN(A, B)			((A) < (B) ? (A) : (B))
+#define CLEANMASK(mask)		(mask & ~WLR_MODIFIER_CAPS)
+#define VISIBLEON(C, M)		((C)->mon == (M) && ((C)->tags & (M)->tagset[(M)->seltags]))
+#define LENGTH(X)			(sizeof X / sizeof X[0])
+#define END(A)				((A) + LENGTH(A))
+#define TAGMASK				((1 << LENGTH(tags)) - 1)
+#define ROUND(X)			((int)((X)+0.5))
+#define LISTEN(E, L, H)		wl_signal_add((E), ((L)->notify = (H), (L)))
 
 /* enums */
 enum { CurNormal, CurMove, CurResize }; /* cursor */
