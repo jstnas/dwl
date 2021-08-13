875206ee09feaf44bdd7495283187c78252b38a6
Justinas Grigas <jstn_as@protonmail.com>
2021-08-13 11:29:39 +0100

    added hotkeys for firefox
    firefox can now be launched in two different profiles.
    layout hotkeys now se shift.

--- config.def.h
+++ config.def.h
@@ -66,6 +66,8 @@ static const char *termcmd[] = { "st", "fish", NULL };
 static const char *menucmd[] = { "dmenu-wl_run", NULL };
 static const char *volup[] = { "/home/iota/.local/bin/change_volume", "+", NULL };
 static const char *voldn[] = { "/home/iota/.local/bin/change_volume", "-", NULL };
+static const char *firefoxcmd[] = { "firefox", "-P", "custom", NULL };
+static const char *firefoxaltcmd[] = { "firefox", "-P", "default", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
@@ -81,9 +83,9 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
 	{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
-	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_T,          setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_F,          setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_M,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
 	{ MODKEY,                    XKB_KEY_e,          togglefullscreen, {0} },
@@ -105,6 +107,8 @@ static const Key keys[] = {
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
 	{ 0, XKB_KEY_XF86AudioRaiseVolume,               spawn,          {.v = volup} },
 	{ 0, XKB_KEY_XF86AudioLowerVolume,               spawn,          {.v = voldn} },
+	{ MODKEY,                    XKB_KEY_f,          spawn,          {.v = firefoxcmd} },
+	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_f,          spawn,          {.v = firefoxaltcmd} },
 
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
