5bcc3cd22ac677e490de1a0d1249ea2b9ef444cb
Justinas Grigas <jstn_as@protonmail.com>
2021-09-28 07:12:10 +0100

    moved colors to seperate header file

--- config.def.h
+++ config.def.h
@@ -1,10 +1,7 @@
 /* appearance */
 static const int sloppyfocus        = 1;  /* focus follows mouse */
 static const unsigned int borderpx  = 2;  /* border pixel of windows */
-static const float rootcolor[]      = {0, 0.0625, 0.125, 1.0};
-static const float bordercolor[]    = {0.1875, 0.25, 0.3125, 1.0};
-static const float focuscolor[]     = {0.4375, 0.25, 0.125, 1.0};
-
+#include "fellow_colors.h"
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
---
+++ fellow_colors.h
@@ -0,0 +1,3 @@
+static const float rootcolor[]      = {0, 0.0625, 0.125, 1.0};
+static const float bordercolor[]    = {0.1875, 0.25, 0.3125, 1.0};
+static const float focuscolor[]     = {0.4375, 0.25, 0.125, 1.0};
